0d4324c6721081aa9b805e39b0d246219e5fdca8
nophono <codpilot2022@proton.me>
2026-07-06 04:34:01 +0200

    remove debugging members

--- shared/mapping.h
+++ shared/mapping.h
@@ -13,19 +13,13 @@ typedef struct {
   void *get_module_handle_a;
 
   void *pbase;
-  uint8_t loader_ran;
+
   uint8_t success;
   uint32_t fwd_reason_param;
   void *reserved_param;
   uint8_t seh_support;
 
-  uint32_t stage;
 
-  void *last_import_desc;
-  void *last_thunk;
-  void *last_module_name;
-  void *last_function_name;
-  void *last_resolved;
 
 
   void *rtl_add_function_table; // goes unused in x32
